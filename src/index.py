4048aa8ca1f0a0a977593d334844d3e22f733d6f
PavPavv <pavpavdev@gmail.com>
2022-12-13 12:12:32 +0400

    feat: add 3 more jr solutions

--- src/index.py
+++ src/index.py
@@ -19,3 +19,12 @@ print(firstRight([1,2,3,1,2,3],3))
 def isPalindrome(s):
   return s.lower() == s.lower()[::-1]
 print(isPalindrome('Anna'))
+
+# 4
+def iterateFromNumToNum(a,b):
+  counter = 0
+  for x in range(a,b+1):
+    counter += x
+  return counter
+print(iterateFromNumToNum(1,5))
+print(iterateFromNumToNum(3,6))
