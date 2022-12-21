2e9e7557bad3a79d7f19ffb247d83ac4306151d7
PavPavv <pavpavdev@gmail.com>
2022-12-21 17:11:47 +0400

    feat: add 4 more jr solutions

--- src/index.py
+++ src/index.py
@@ -21,6 +21,25 @@ def isPalindrome(s):
 print(isPalindrome('Anna'))
 
 # 4
+def findTargetListEl(arr, lenNum):
+  result = []
+  for el in arr:
+    if len(el) == lenNum:
+      result.append(el)
+  return result
+print(findTargetListEl(['Mark', 'John', 'Ann'],4))  # ['Mark', 'John']
+
+# 5
+def countVowels(s):
+  counter = 0
+  vowels = ['a','e','i','o','u']
+  for ch in s:
+    if ch in vowels:
+      counter += 1
+  return counter
+print(countVowels('wolf'))
+
+# 6
 def iterateFromNumToNum(a,b):
   counter = 0
   for x in range(a,b+1):
@@ -28,3 +47,25 @@ def iterateFromNumToNum(a,b):
   return counter
 print(iterateFromNumToNum(1,5))
 print(iterateFromNumToNum(3,6))
+
+# 7
+def findMidChar(s):
+  isEven = len(s) % 2 == 0
+  isOdd = len(s) % 2 == 0
+  if isEven:
+    return s[int(len(s) / 2)]
+  if isOdd:
+    return s[int((len(s) - 1) / 2)]
+print(findMidChar('test'))
+print(findMidChar('testing'))
+
+# 8
+def squareNum(n):
+  result = ''
+  slist = list(format(n))
+  for s in  slist:
+    squareNum = int(s)*int(s)
+    result += format(squareNum)
+  return result
+print(squareNum(25))  # 425
+
