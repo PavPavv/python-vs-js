21814949b9e93998f88bbf468008cb72abf4106d
PavPavv <pavpavdev@gmail.com>
2023-01-04 16:38:53 +0400

    feat: add twoMax and findMinEven solutions

--- src/index.py
+++ src/index.py
@@ -204,3 +204,29 @@ def findPopularChar(str):
       result = char
   return result
 print(findPopularChar('ababa'))  # 'a'
+
+# 21
+def twoMax(arr):
+  max1 = max(arr[0], arr[1])
+  max2 = min(arr[0], arr[1])
+  for n in arr[2:]:
+    if n > max1:
+      max2 = max1
+      max1 = n
+    elif n > max2:
+      max2 = n
+  return [max1, max2]
+
+print(twoMax([10,-2,1,2,3,4,5]))  # [10,5]
+
+# 22
+def findMinEven(arr):
+  flag = False
+  result = -1
+  for i in range(len(arr)):
+    if arr[i] % 2 == 0 and (not flag or arr[i] < result):
+      result = arr[i]
+      flag = True
+  return result
+print(findMinEven([10,-2,1,2,3,4,5]))
+print(findMinEven([10,12,1,2,3,4,5]))
